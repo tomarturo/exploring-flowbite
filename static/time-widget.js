a43fde9084ba6ea3f23d25ceb08f3b407178f99c
Tom Kurzeka <tomarturo@Toms-Big-Macbook-Pro.local>
2023-11-02 21:56:41 -0700

    updates to 24 hour time

--- static/time-widget.js
+++ static/time-widget.js
@@ -8,36 +8,16 @@ function setCurrentTime() {
     // Adjust for UTC-8 by adding 8 hours (in minutes)
     myDate.setMinutes(myDate.getMinutes() + 8 * 60 - utcOffset);
 
-    let daysList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let monthsList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-    let date = myDate.getDate();
-    let month = monthsList[myDate.getMonth()];
-    let year = myDate.getFullYear();
-    let day = daysList[myDate.getDay()];
-
     // Function to add a '0' before single digits
     function addLeadingZero(num) {
         return num < 10 ? '0' + num : num;
     }
 
-    let amOrPm;
-    let twelveHours = function() {
-        if (myDate.getHours() > 12) {
-            amOrPm = 'PM';
-            let twentyFourHourTime = myDate.getHours();
-            let conversion = twentyFourHourTime - 12;
-            return `${addLeadingZero(conversion)}`;
-        } else {
-            amOrPm = 'AM';
-            return `${addLeadingZero(myDate.getHours())}`;
-        }
-    };
-    let hours = twelveHours();
+    let hours = addLeadingZero(myDate.getHours());
     let minutes = addLeadingZero(myDate.getMinutes());
     let seconds = addLeadingZero(myDate.getSeconds());
 
-    let currentTime = `${hours}:${minutes}:${seconds} ${amOrPm}`;
+    let currentTime = `${hours}:${minutes}:${seconds}`;
 
     document.getElementById('current-time').innerText = currentTime;
 }
